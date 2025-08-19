f4663cd61c0e378d5f8383a07f6760088e1cb3a2
carlos <carferlome22@gmail.com>
2025-08-19 13:58:20 -0500

    modified:   Agropulp.html 	modified:   contacto.html 	new file:   js/formulario.js 	modified:   style/main.css

---
+++ js/formulario.js
@@ -0,0 +1,19 @@
+  (function () {
+    emailjs.init("5qEcEu8YLFKxa-3dV"); // Reemplaza con tu Public Key
+  })();
+
+  document
+    .getElementById("contact-form")
+    .addEventListener("submit", function (e) {
+      e.preventDefault();
+
+      emailjs.sendForm("service_53qbo5a", "template_7z0b5h9", this).then(
+        function () {
+          alert("✅ Mensaje enviado correctamente");
+          document.getElementById("contact-form").reset();
+        },
+        function (error) {
+          alert("❌ Error al enviar: " + JSON.stringify(error));
+        }
+      );
+    });
